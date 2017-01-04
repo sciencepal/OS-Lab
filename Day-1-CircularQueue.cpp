9fa0c158b4d568840bfe31c952c74749ce07209c
Anirudh Kanabar <anirudhkanabar@gmail.com>
2017-01-04 09:54:33 +0530

    Created Day-1-CurcularQueue.cpp
    A Queue implemented using doubly linked-list

---
+++ Day-1-CircularQueue.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+class CircularQueue
+{
+    struct Node
+    {
+        int n;
+        Node *next,*prev;
+    };
+    Node *front,*end;
+    Node* createNewNode(int x)
+    {
+        Node *t = new Node();
+        t->n = x;
+        t->next = t->prev =NULL;
+    }
+
+    void insertNewNode(Node* t)
+    {
+        if(front==end && end==NULL)
+        {
+            front = t;
+            end = t;
+        }
+        else
+        {
+            t->next = end;
+            end->prev = t;
+            end = t;
+        }
+    }
+
+    void deleteNode()
+    {
+        Node *temp = front;
+        front = front->prev;
+        delete temp;
+    }
+public:
+    CircularQueue()
+    {
+        front = NULL;
+        end = NULL;
+    }
+    void enqueue(int x)
+    {
+        insertNewNode(createNewNode(x));
+    }
+    void dequeue()
+    {
+        deleteNode();
+    }
+    void displayQueue()
+    {
+        Node *t = end;
+        while(true)
+        {
+            cout<<t->n<<"->";
+            if(t!=front)
+            {
+                t=t->next;
+            }
+            else
+            {
+                cout<<"!!!"<<endl;
+                break;
+            }
+        }
+    }
+};
+
+int main()
+{
+    CircularQueue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.displayQueue();
+    q.dequeue();
+    q.displayQueue();
+    q.enqueue(5);
+    q.displayQueue();
+    q.dequeue();
+    q.displayQueue();
+    return 0;
+}
